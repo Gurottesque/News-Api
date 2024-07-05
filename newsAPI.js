960741a619f97972596324fdbcf4e336c877d5c9
Gurottesque <a.bedoya3@utp.edu.co>
2024-07-04 21:19:06 -0500

    Update and rename TMDBApi.js to newsAPI.js

--- newsAPI.js
+++ newsAPI.js
@@ -1,17 +1,14 @@
 import express from 'express';
 import api from './api/api.js';
-import dotenv from 'dotenv';
 import articles from './endpoints/articles.js';
-
-dotenv.config();
 
 const app = express();
 const port = 3000;
-const KEY = process.env.KEY;
 
 app.use('/api/articles', articles)
 
 app.get('/api/breaking-events', async (req, res) => {
+    
     const params_api = {
         page : req.query.page,
         region : req.query.region,
